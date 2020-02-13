493d9cc96cacca51acdfcf04dfb8ca8493ba62e8
Matthijs Kamstra <matthijs@fonk.amsterdam>
2020-02-13 11:20:12 +0100

    update make working for with dat.gui and sketcher

--- src/art/CC100.hx
+++ src/art/CC100.hx
@@ -20,6 +20,23 @@ class CC100 extends SketcherBase {
 	// animate
 	var dot:Circle;
 
+	// dat
+	var message = 'dat.gui';
+	var speed = 0.8;
+	var displayOutline = false;
+	var noiseStrength = 0.8;
+	var growthSpeed = 0.8;
+	var maxSize = 0.8;
+	var explode = function() {
+		trace('booom');
+	};
+
+	var color0 = "#ffae23"; // CSS string
+	var color1 = [0, 128, 255]; // RGB array
+	var color2 = [0, 128, 255, 0.3]; // RGB with alpha
+	var color3 = {h: 350, s: 0.9, v: 0.3}; // Hue, saturation, value
+
+	///
 	var fontFamly = 'Oswald:200,300,400,500,600,700';
 
 	public function new() {
@@ -27,16 +44,60 @@ class CC100 extends SketcherBase {
 		this.isDebug = true;
 
 		super();
-		// embed files and then init
-		EmbedUtil.embedGoogleFont(fontFamly, init);
+		// embed font files and then init
+		EmbedUtil.embedGoogleFont(fontFamly, drawShape);
 		// init
+		// embed dat.GUI and init
+		EmbedUtil.datgui(initDatGui2);
+		// initDatGui();
 	}
 
-	function init() {
-		dot = createShape(100, {x: w2, y: h2});
-		createQuickSettings();
-		onAnimateHandler(dot);
-		drawShape();
+	function initDatGui2() {
+		var text = new FizzyText();
+		var gui = new js.dat.gui.GUI();
+		gui.add(text, 'message');
+		gui.add(text, 'speed', -5, 5);
+		gui.add(text, 'displayOutline');
+		gui.add(text, 'explode');
+	}
+
+	function initDatGui() {
+		var gui = new js.dat.gui.GUI();
+		gui.add(this, 'message');
+		gui.add(this, 'speed', -5, 5);
+		gui.add(this, 'displayOutline');
+		gui.add(this, 'explode');
+
+		var f1 = gui.addFolder('Flow Field');
+		f1.add(this, 'speed');
+		f1.add(this, 'noiseStrength');
+
+		var f2 = gui.addFolder('Letters');
+		f2.add(this, 'growthSpeed');
+		f2.add(this, 'maxSize');
+		f2.add(this, 'message');
+
+		// Choose from accepted values
+		gui.add(this, 'message', ['pizza', 'chrome', 'hooray']);
+
+		// // Choose from named values
+		gui.add(this, 'speed', {Stopped: 0, Slow: 0.1, Fast: 5});
+
+		gui.addColor(this, 'color0');
+		gui.addColor(this, 'color1');
+		gui.addColor(this, 'color2');
+		gui.addColor(this, 'color3');
+
+		var controller = gui.add(this, 'maxSize', 0, 10);
+		controller.onChange(function(value) {
+			// Fires on every change, drag, keypress, etc.
+			trace('value: $value');
+		});
+
+		controller.onFinishChange(function(value) {
+			// Fires when a controller loses focus.
+			js.Browser.alert("The new value is " + value);
+		});
 	}
 
 	function createQuickSettings() {
@@ -79,6 +140,9 @@ class CC100 extends SketcherBase {
 	}
 
 	function drawShape() {
+		if (this.dot == null)
+			return;
+
 		// reset previous sketch
 		sketch.clear();
 
@@ -89,27 +153,29 @@ class CC100 extends SketcherBase {
 		// group
 		var bgGroup = sketch.makeGroup([bg]); // , bg1
 		bgGroup.id = "sketch background";
-		bgGroup.fill = getColourObj(BLACK); // BLACK
+		bgGroup.fill = getColourObj(_color1); // BLACK
 
 		// quick generate grid
 		if (isDebug) {
 			sketcher.debug.Grid.gridDots(sketch, grid);
 		}
 
+		// dummy loop to generate correct number of shapes
 		for (i in 0...shapeArray.length) {
 			var sh = shapeArray[i];
 		}
-		// var rgb = randomColourObject();
-		// ctx.strokeColour(rgb.r, rgb.g, rgb.b);
-		// ctx.xcross(w/2, h/2, 200);
 
 		var text = sketch.makeText(toString(), w2, h2);
 		text.fontFamily = fontFamly;
+		text.fontSizePx = 100;
+		text.fontWeight = '800';
+		text.textAlign = TextAlignType.Center;
 		text.fillColor = getColourObj(_color4);
 
-		var circle = sketch.makeCircle(dot.x, dot.y, 20);
-		circle.strokeWeight = 2;
+		var circle = sketch.makeCircle(dot.x, dot.y, 50);
+		circle.strokeWeight = 10;
 		circle.strokeColor = getColourObj(_color3);
+		circle.fillOpacity = 0;
 
 		// update sketch, to draw svg or canvas
 		sketch.update();
@@ -119,6 +185,10 @@ class CC100 extends SketcherBase {
 
 	override function setup() {
 		trace('SETUP :: ${toString()}');
+
+		dot = createShape(100, {x: w2, y: h2});
+		createQuickSettings();
+		onAnimateHandler(dot);
 
 		var colorArray = ColorUtil.niceColor100SortedString[randomInt(ColorUtil.niceColor100SortedString.length - 1)];
 		_color0 = hex2RGB(colorArray[0]);
@@ -146,4 +216,15 @@ class CC100 extends SketcherBase {
 		drawShape();
 		// stop();
 	}
+}
+
+class FizzyText {
+	public var message = 'dat.gui';
+	public var speed = 0.8;
+	public var displayOutline = false;
+	public var explode = function() {
+		trace("BOOM");
+	};
+
+	public function new() {}
 }
